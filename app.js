3eaa9163da7494eca9cbf4a16db07068ff853081
Gina <gkjarvis@mail.fhsu.edu>
2018-08-25 18:19:13 -0600

    last update of js

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 console.log ("hi")
-"This is some more code to save and push to the file "
+newFunction(); "will this autosave"
+function newFunction() {
+  "This is some more code to save and push to the file ";
+}
+
